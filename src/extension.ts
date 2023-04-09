b606f6976dd0181a0987dc3fd47a056604325104
Vadym Parakonnyi <v.parakonnyi@gmail.com>
2023-04-09 17:43:20 +0200

    feat(parser): add lsp server and use peggy parser (WIP)

--- src/extension.ts
+++ src/extension.ts
@@ -1,223 +1,24 @@
 import * as vscode from 'vscode';
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-ignore
-import { SBVRParser } from '@balena/sbvr-parser';
-
-const tokenTypes = new Map<string, number>();
-const tokenModifiers = new Map<string, number>();
-
-const legend = (function () {
-  const tokenTypesLegend = [
-    'comment',
-    'string',
-    'keyword',
-    'number',
-    'regexp',
-    'operator',
-    'namespace',
-    'type',
-    'struct',
-    'class',
-    'interface',
-    'enum',
-    'typeParameter',
-    'function',
-    'method',
-    'decorator',
-    'macro',
-    'variable',
-    'parameter',
-    'property',
-    'label',
-  ];
-  tokenTypesLegend.forEach((tokenType, index) =>
-    tokenTypes.set(tokenType, index)
-  );
-
-  const tokenModifiersLegend = [
-    'declaration',
-    'documentation',
-    'readonly',
-    'static',
-    'abstract',
-    'deprecated',
-    'modification',
-    'async',
-  ];
-  tokenModifiersLegend.forEach((tokenModifier, index) =>
-    tokenModifiers.set(tokenModifier, index)
-  );
-
-  return new vscode.SemanticTokensLegend(
-    tokenTypesLegend,
-    tokenModifiersLegend
-  );
-})();
+import { LanguageClient, TransportKind } from 'vscode-languageclient/node';
 
 export function activate(context: vscode.ExtensionContext) {
-  const tokensProvider =
-    vscode.languages.registerDocumentSemanticTokensProvider(
-      { language: 'sbvr' },
-      new SbvrTokensProvider(),
-      legend
-    );
-  context.subscriptions.push(tokensProvider);
 
-  const definitionProvider = vscode.languages.registerDefinitionProvider(
+  const serverModule = context.asAbsolutePath('out/src/server.js');
+  const client = new LanguageClient(
     'sbvr',
-    new SbvrDefinitionProvider()
+    'sbvr',
+    {
+      run: { module: serverModule, transport: TransportKind.ipc },
+      debug: {
+        module: serverModule,
+        transport: TransportKind.ipc,
+        options: { execArgv: ['--nolazy', '--inspect=6009'] },
+      },
+    },
+    {
+      documentSelector: [{ language: 'sbvr', scheme: 'file' }],
+    }
   );
-  context.subscriptions.push(definitionProvider);
-
-  const documentSymbolProvider =
-    vscode.languages.registerDocumentSymbolProvider(
-      { language: 'sbvr' },
-      new SbvrDocumentSymbolProvider()
-    );
-  context.subscriptions.push(documentSymbolProvider);
-}
-
-interface IParsedToken {
-  line: number;
-  startCharacter: number;
-  length: number;
-  tokenType: string;
-  tokenModifiers: string[];
-}
-
-class SbvrTokensProvider implements vscode.DocumentSemanticTokensProvider {
-  async provideDocumentSemanticTokens(
-    document: vscode.TextDocument,
-    token: vscode.CancellationToken
-  ): Promise<vscode.SemanticTokens> {
-    const allTokens = this._parseText(document.getText());
-    const builder = new vscode.SemanticTokensBuilder();
-    allTokens.forEach(token => {
-      builder.push(
-        token.line,
-        token.startCharacter,
-        token.length,
-        this._encodeTokenType(token.tokenType),
-        this._encodeTokenModifiers(token.tokenModifiers)
-      );
-    });
-    return builder.build();
-  }
-
-  private _encodeTokenType(tokenType: string): number {
-    if (tokenTypes.has(tokenType)) {
-      return tokenTypes.get(tokenType)!;
-    } else if (tokenType === 'notInLegend') {
-      return tokenTypes.size + 2;
-    }
-    return 0;
-  }
-
-  private _encodeTokenModifiers(strTokenModifiers: string[]): number {
-    let result = 0;
-    for (let i = 0; i < strTokenModifiers.length; i++) {
-      const tokenModifier = strTokenModifiers[i];
-      if (tokenModifiers.has(tokenModifier)) {
-        result = result | (1 << tokenModifiers.get(tokenModifier)!);
-      } else if (tokenModifier === 'notInLegend') {
-        result = result | (1 << (tokenModifiers.size + 2));
-      }
-    }
-    return result;
-  }
-
-  private _parseText(text: string): IParsedToken[] {
-    const r: IParsedToken[] = [];
-    const lines = text.split(/\r\n|\r|\n/);
-    for (let i = 0; i < lines.length; i++) {
-      const line = lines[i];
-      let currentOffset = 0;
-      do {
-        const openOffset = line.indexOf('[', currentOffset);
-        if (openOffset === -1) {
-          break;
-        }
-        const closeOffset = line.indexOf(']', openOffset);
-        if (closeOffset === -1) {
-          break;
-        }
-        const tokenData = this._parseTextToken(
-          line.substring(openOffset + 1, closeOffset)
-        );
-        r.push({
-          line: i,
-          startCharacter: openOffset + 1,
-          length: closeOffset - openOffset - 1,
-          tokenType: tokenData.tokenType,
-          tokenModifiers: tokenData.tokenModifiers,
-        });
-        currentOffset = closeOffset;
-      } while (true);
-    }
-    return r;
-  }
-
-  private _parseTextToken(text: string): {
-    tokenType: string;
-    tokenModifiers: string[];
-  } {
-    const parts = text.split('.');
-    return {
-      tokenType: parts[0],
-      tokenModifiers: parts.slice(1),
-    };
-  }
-}
-
-class SbvrDefinitionProvider implements vscode.DefinitionProvider {
-  provideDefinition(
-    document: vscode.TextDocument,
-    position: vscode.Position,
-    token: vscode.CancellationToken
-  ): vscode.ProviderResult<vscode.Definition> {
-    return null;
-  }
-}
-
-class SbvrDocumentSymbolProvider implements vscode.DocumentSymbolProvider {
-  private format(cmd: string): string {
-    return cmd
-      .substr(1)
-      .toLowerCase()
-      .replace(/^\w/, c => c.toUpperCase());
-  }
-
-  public provideDocumentSymbols(
-    document: vscode.TextDocument,
-    token: vscode.CancellationToken
-  ): Promise<vscode.DocumentSymbol[]> {
-    return new Promise((resolve, reject) => {
-      const symbols: vscode.DocumentSymbol[] = [];
-      const nodes = [symbols];
-      const inside_obj = false;
-      const inside_prop = false;
-      const inside_userinput = false;
-
-      const symbolkind_obj = vscode.SymbolKind.Object;
-      const symbolkind_prop = vscode.SymbolKind.Property;
-      const symbolkind_mtd = vscode.SymbolKind.Method;
-
-      /*   const marker_symbol = new vscode.DocumentSymbol(
-        'Marker',
-        'Component',
-        symbolkind_obj,
-        line.range,
-        line.range
-      ); */
-
-      const parser = SBVRParser.createInstance();
-      const text = document.getText();
-      parser.setInput(text);
-      const LF = parser.matchAll(text, 'Process');
-
-      // console.log(parser.lines);
-
-      resolve(symbols);
-    });
-  }
+  context.subscriptions.push(client);
+  client.start();
 }
---
+++ src/server.ts
@@ -0,0 +1,360 @@
+import * as lsp from 'vscode-languageserver/node';
+import { TextDocument } from 'vscode-languageserver-textdocument';
+import { SbvrPeggyParser } from './parser';
+import {
+  toLSPRange,
+  containsPosition,
+  escapeMarkdown,
+  EasySemanticTokensBuilder,
+  isBefore,
+  isBeforeOrEqual,
+  replaceWhitespaceLeft,
+  toLSPPosition,
+  replaceWhitespaceRight,
+} from './utils';
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+import { SBVRParser } from '@balena/sbvr-parser';
+
+const conn = lsp.createConnection(lsp.ProposedFeatures.all);
+const documents: lsp.TextDocuments<TextDocument> = new lsp.TextDocuments(
+  TextDocument
+);
+
+const legend: lsp.SemanticTokensLegend = {
+  tokenTypes: ['number', 'sbvr', 'string', 'comment', 'keyword'],
+  tokenModifiers: [],
+};
+const tokenTypeMap = new Map<string, number>(
+  [...legend.tokenTypes.values()].map((v, i) => [v, i])
+);
+
+const parser = new SbvrPeggyParser();
+
+const docs: any[] = [];
+
+conn.onInitialize(params => {
+  return {
+    capabilities: {
+      textDocumentSync: lsp.TextDocumentSyncKind.Incremental,
+      semanticTokensProvider: {
+        documentSelector: [{ language: 'sbvr' }],
+        legend,
+        full: true,
+        range: false,
+      },
+      hoverProvider: true,
+      completionProvider: { triggerCharacters: [' ', '-', '!'] },
+      documentFormattingProvider: true,
+      workspace: {
+        workspaceFolders: {
+          supported: true,
+        },
+      },
+    },
+  };
+});
+
+const documentSettings = new Map<string, Thenable<unknown>>();
+conn.onInitialized(() => {
+  conn.client.register(lsp.DidChangeConfigurationNotification.type, undefined);
+});
+conn.onDidChangeConfiguration(change => {
+  documentSettings.clear();
+});
+documents.onDidClose(e => {
+  documentSettings.delete(e.document.uri);
+});
+const getConfiguration = async (uri: string): Promise<any> => {
+  if (documentSettings.has(uri)) {
+    return documentSettings.get(uri);
+  }
+  const result = await conn.workspace.getConfiguration({
+    section: 'sbvr',
+    scopeUri: uri,
+  });
+  documentSettings.set(uri, result);
+  return result;
+};
+
+const isSbvrFile = (textDocument: TextDocument) =>
+  textDocument.languageId === 'sbvr' || textDocument.uri.endsWith(`.sbvr`);
+
+const check = (textDocument: TextDocument) => {
+  console.log('DEBUG ~ file: server.ts:84 ~ check ~ textDocument:', textDocument);
+
+  if (!isSbvrFile(textDocument)) {
+    conn.sendDiagnostics({ uri: textDocument.uri, diagnostics: [] });
+  } else {
+    const err = parser.check(textDocument.getText());
+    conn.sendDiagnostics({
+      uri: textDocument.uri,
+      diagnostics:
+        err === null
+          ? []
+          : [
+              {
+                range: toLSPRange(err.location),
+                message: err.message,
+                severity: lsp.DiagnosticSeverity.Error,
+              },
+            ],
+    });
+  }
+};
+
+documents.onDidOpen(({ document }) => {
+  check(document);
+});
+documents.onDidChangeContent(({ document }) => {
+  check(document);
+});
+
+conn.languages.semanticTokens.on(({ textDocument: { uri } }) => {
+  console.log(
+    'DEBUG ~ file: server.ts:112 ~ conn.languages.semanticTokens.on ~ uri:',
+    uri
+  );
+
+  const textDocument = documents.get(uri);
+  if (textDocument === undefined || !isSbvrFile(textDocument)) {
+    return new lsp.SemanticTokensBuilder().build();
+  }
+  const text = textDocument.getText();
+  const lines = text.split('\n');
+  const builder = new EasySemanticTokensBuilder(lines, tokenTypeMap);
+
+  const f = parser.parse(text);
+  if (f !== null) {
+    for (const line of f) {
+      if (line.type === 'Term') {
+        for (const attr of line.attrs) {
+          builder.push(
+            toLSPRange(attr.key.location),
+            docs.find(d => d.title === attr.key.text) ? 'sbvr' : 'keyword'
+          );
+          if (attr.value !== null) {
+            builder.push(toLSPRange(attr.value.location), 'string');
+          }
+        }
+      } else if (line.type === 'comment') {
+        builder.push(toLSPRange(line.comment.location), 'comment');
+      } else {
+        builder.push(
+          toLSPRange((line satisfies { type: 'macro' }).header.location),
+          'keyword'
+        );
+      }
+    }
+  }
+
+  return builder.build();
+});
+
+conn.onDocumentFormatting(
+  async ({
+    options,
+    textDocument: { uri },
+  }): Promise<lsp.TextEdit[] | null> => {
+    const textDocument = documents.get(uri);
+    if (textDocument === undefined || !isSbvrFile(textDocument)) {
+      return null;
+    }
+    const text = textDocument.getText();
+    const f = parser.parse(textDocument.getText());
+    if (f === null) {
+      return null;
+    }
+    const edits: lsp.TextEdit[] = [];
+    for (const line of f!) {
+      if (line.type === 'pattern') {
+        // `  key    attrs` -> `key    attrs`
+        edits.push(
+          ...replaceWhitespaceLeft(
+            toLSPPosition(line.pattern.location.start).offset,
+            '',
+            text,
+            textDocument
+          )
+        );
+
+        if (line.attrs.length > 0) {
+          let tabSize = ((await getConfiguration(uri)) as any).format
+            .gitattributes.tabSize;
+          if (typeof tabSize !== 'number') {
+            tabSize = 11;
+          }
+          let hardTab = ((await getConfiguration(uri)) as any).format
+            .gitattributes.hardTab;
+          if (typeof hardTab !== 'boolean') {
+            hardTab = false;
+          }
+          let hardTabWidth = ((await getConfiguration(uri)) as any).format
+            .gitattributes.hardTabWidth;
+          if (typeof hardTabWidth !== 'number') {
+            hardTabWidth = 4;
+          }
+
+          const tab = hardTab
+            ? '\t'.repeat(
+                Math.max(
+                  1,
+                  (tabSize as number) -
+                    Math.floor(
+                      (toLSPPosition(line.pattern.location.end).character /
+                        hardTabWidth) as number
+                    )
+                )
+              )
+            : ' '.repeat(
+                Math.max(
+                  1,
+                  (tabSize as number) -
+                    toLSPPosition(line.pattern.location.end).character
+                )
+              );
+          // `key attrs` -> `key    attrs`
+          edits.push(
+            ...replaceWhitespaceLeft(
+              toLSPPosition(
+                (line.attrs[0].operator ?? line.attrs[0].key).location.start
+              ).offset,
+              tab,
+              text,
+              textDocument
+            )
+          );
+          // `key    attrs  ` -> `key    attrs`
+          edits.push(
+            ...replaceWhitespaceRight(
+              toLSPPosition(
+                (
+                  line.attrs[line.attrs.length - 1].value ??
+                  line.attrs[line.attrs.length - 1].key
+                ).location.end
+              ).offset,
+              '',
+              text,
+              textDocument
+            )
+          );
+        }
+      } else if (line.type === 'macro') {
+        edits.push(
+          ...replaceWhitespaceLeft(
+            toLSPPosition(line.location.start).offset,
+            '',
+            text,
+            textDocument
+          )
+        );
+      } else {
+        line.type satisfies 'comment';
+      }
+    }
+
+    return edits;
+  }
+);
+
+conn.onHover(({ position, textDocument: { uri } }) => {
+  const textDocument = documents.get(uri);
+  if (textDocument === undefined || !isSbvrFile(textDocument)) {
+    return;
+  }
+  const f = parser.parse(textDocument.getText());
+  if (f === null) {
+    return;
+  }
+  const newHover = (value: string, range: lsp.Range): lsp.Hover => ({
+    contents: { kind: lsp.MarkupKind.Markdown, value },
+    range,
+  });
+  for (const line of f) {
+    if (line.type === 'pattern') {
+      for (const attr of line.attrs) {
+        const range = toLSPRange(attr.key.location);
+        if (containsPosition(range, position)) {
+          if (attr.key.text === 'binary') {
+            // built-in macro
+            return newHover(
+              `\`\`\`\n${escapeMarkdown(
+                attr.key.text
+              )} (built-in macro)\n\`\`\`\n\n---\nExpands to \`-text -diff\``,
+              range
+            );
+          }
+          const documentation = docs.find(d => d.title === attr.key.text);
+          if (documentation !== undefined) {
+            // built-in attribute
+            return newHover(
+              `\`\`\`\n${escapeMarkdown(attr.key.text)}\n\`\`\`\n\n---\n${
+                documentation.documentation
+              }`,
+              range
+            );
+          }
+          // user-defined macro
+          // TODO: expand user-defined macros
+          return newHover(
+            `\`\`\`\n${escapeMarkdown(attr.key.text)} (macro)\n\`\`\`\n`,
+            range
+          );
+        }
+      }
+    } else if (line.type === 'comment') {
+      //
+    } else {
+      line.type satisfies 'macro';
+    }
+  }
+});
+
+conn.onCompletion(({ position, textDocument: { uri } }) => {
+  const textDocument = documents.get(uri);
+  if (textDocument === undefined || !isSbvrFile(textDocument)) {
+    return;
+  }
+  const text = textDocument.getText();
+  const f = parser.parse(text);
+
+  if (f !== null) {
+    for (const line of f) {
+      if (!containsPosition(toLSPRange(line.location), position)) {
+        continue;
+      }
+      if (line.type === 'pattern') {
+        if (isBeforeOrEqual(position, toLSPRange(line.pattern.location).end)) {
+          return;
+        }
+        for (const attr of line.attrs) {
+          if (
+            attr.value !== null &&
+            containsPosition(toLSPRange(attr.value.location), position)
+          ) {
+            return;
+          }
+        }
+        return [
+          ...docs.map(
+            (d): lsp.CompletionItem => ({
+              label: d.title,
+              kind: lsp.CompletionItemKind.Property,
+              documentation: d.documentation,
+            })
+          ),
+          {
+            label: 'binary',
+            kind: lsp.CompletionItemKind.Keyword,
+            documentation: 'Expands  to `-text -diff`',
+          },
+        ];
+      } else {
+        line.type satisfies 'macro' | 'comment';
+      }
+    }
+  }
+});
+
+documents.listen(conn);
+conn.listen();
